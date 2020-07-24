f51c47f5ed8e99beae768122bd6fa878387bc328
adlered <adlered@outlook.com>
2020-07-24 21:29:41 +0800

    认证

--- src/main/java/pers/adlered/bce/ddns/BaiduCloud.java
+++ src/main/java/pers/adlered/bce/ddns/BaiduCloud.java
@@ -2,11 +2,17 @@ package pers.adlered.bce.ddns;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * <h3>bce_ddns</h3>
@@ -28,7 +34,11 @@ public class BaiduCloud {
         // AccessKeyId
         setAuthStringPrefix("accessKeyId", AK);
         // TimeStamp
-        String timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(new Date());
+        Calendar calendar = Calendar.getInstance();
+        int zoneOffset = calendar.get(Calendar.ZONE_OFFSET);
+        int dstOffset = calendar.get(Calendar.DST_OFFSET);
+        calendar.add(Calendar.MILLISECOND, -(zoneOffset + dstOffset));
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(new Date(calendar.getTimeInMillis()));
         setAuthStringPrefix("timestamp", timestamp);
         // ExpirationPeriodInSeconds
         setAuthStringPrefix("expirationPeriodInSeconds", 3600);
@@ -44,13 +54,14 @@ public class BaiduCloud {
         canonicalUri = canonicalUri.replaceAll("%2F", "/");
         setCanonicalRequest("canonicalURI", canonicalUri);
         // CanonicalQueryString
-        setCanonicalRequest("canonicalQueryString", "" +
-                URLEncoder.encode("domain") + "=" + URLEncoder.encode("stackoverflow.wiki", "UTF-8"));
+        //setCanonicalRequest("canonicalQueryString", "" +
+         //       URLEncoder.encode("domain") + "=" + URLEncoder.encode("stackoverflow.wiki", "UTF-8"));
+        setCanonicalRequest("canonicalQueryString", "");
         // CanonicalHeaders
         String canonicalHeaders = "host:bcd.baidubce.com\nx-bce-date:" + timestamp;
         setCanonicalRequest("canonicalHeaders", canonicalHeaders);
         // SignedHeaders 签名头域
-        String signedHeaders = "";
+        String signedHeaders = "host;x-bce-date";
         // SigningKey 派生密钥
         String signingKey = hMacSha256(authStringPrefix, SK);
         System.out.println("SigningKey = " + signingKey);
@@ -61,6 +72,8 @@ public class BaiduCloud {
         // ### Authorization 认证字符串 ###
         String authorization = authStringPrefix + "/" + signedHeaders + "/" + signature;
         System.out.println("Authorization = " + authorization);
+
+        sendPost("http://bcd.baidubce.com/v1/domain/resolve/list", "{\n\"domain\" : \"stackoverflow.wiki\"\n}", timestamp, authorization);
     }
 
     private static void setAuthStringPrefix(String key, Object value) {
@@ -74,14 +87,92 @@ public class BaiduCloud {
     }
 
     public static String hMacSha256(String data, String key) throws Exception {
-        Mac sha256Hmac = Mac.getInstance("HmacSHA256");
-        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
-        sha256Hmac.init(secretKey);
-        byte[] array = sha256Hmac.doFinal(data.getBytes("UTF-8"));
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+        byte[] array = sha256_HMAC.doFinal(data.getBytes(StandardCharsets.UTF_8));
         StringBuilder sb = new StringBuilder();
         for (byte item : array) {
             sb.append(Integer.toHexString((item & 0xFF) | 0x100).substring(1, 3));
         }
+
         return sb.toString();
+    }
+
+    public static String sendPost(String url, String param, String time, String authorization) {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String result = "";
+        try {
+            URL realUrl = new URL(url);
+            // 打开和URL之间的连接
+            URLConnection conn = realUrl.openConnection();
+            // 设置通用的请求属性
+            conn.setRequestProperty("Host", "bcd.baidubce.com");
+            conn.setRequestProperty("x-bce-date", time);
+            conn.setRequestProperty("Authorization", authorization);
+            conn.setRequestProperty("User-Agent", "");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "");
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            // 获取URLConnection对象对应的输出流
+            out = new PrintWriter(conn.getOutputStream());
+            // 发送请求参数
+            out.print(param);
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送 POST 请求出现异常！"+e);
+            e.printStackTrace();
+        }
+        //使用finally块来关闭输出流、输入流
+        finally{
+            try{
+                if(out!=null){
+                    out.close();
+                }
+                if(in!=null){
+                    in.close();
+                }
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    public static Date parseUTCText(String text) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        if (text.indexOf(".") > -1) {
+            String prefix = text.substring(0, text.indexOf("."));
+            String suffix = text.substring(text.indexOf("."));
+            if (suffix.length() >= 5) {
+                suffix = suffix.substring(0, 4) + "Z";
+            } else {
+                int len = 5 - suffix.length();
+                String temp = "";
+                temp += suffix.substring(0, suffix.length() - 1);
+                for (int i = 0; i < len; i++) {
+                    temp += "0";
+                }
+                suffix = temp + "Z";
+            }
+            text = prefix + suffix;
+        } else {
+            text = text.substring(0, text.length() - 1) + ".000Z";
+        }
+        Date date = sdf.parse(text);
+        return date;
     }
 }
